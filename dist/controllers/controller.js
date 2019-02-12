714011d3d1447fcd12c26dd770d94490382b739b
mzayed <mariawzayed@gmail.com>
2019-02-12 15:26:01 +0200

    first commit

---
+++ dist/controllers/controller.js
@@ -0,0 +1,51 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//   /lib/controllers/controller.ts
+const mongoose = require("mongoose");
+const model_1 = require("../models/model");
+const contact = mongoose.model('Contact', model_1.ContactSchema);
+class ContactController {
+    addNewContact(req, res) {
+        let newContact = new contact(req.body);
+        newContact.save((err, contact) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    }
+    getContacts(req, res) {
+        contact.find({}, (err, contact) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    }
+    getContactWithID(req, res) {
+        contact.findById(req.params.contactId, (err, contact) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    }
+    updateContact(req, res) {
+        contact.findOneAndUpdate({ _id: req.params.contactId }, req.body, { new: true }, (err, contact) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    }
+    deleteContact(req, res) {
+        contact.remove({ _id: req.params.contactId }, (err) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json({ message: 'Successfully deleted contact!' });
+        });
+    }
+}
+exports.ContactController = ContactController;
+//# sourceMappingURL=controller.js.map
